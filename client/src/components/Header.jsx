32c1156a9fb564cc61820dd5d417bc002edb5233
luka chikashvili <lukachikashvili2020@gmail.com>
2025-05-04 20:05:40 +0400

    add header full design with links

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -4,12 +4,48 @@ import {
     SignedOut,
     UserButton,
   } from '@clerk/nextjs';
-  import { ShoppingCart, User } from 'lucide-react';
+  import { Heart, ShoppingCart, User } from 'lucide-react';
   import React from 'react';
   import logo from '../assets/logo.png';
   import Image from 'next/image';
+import Link from 'next/link';
   
   const Header = () => {
+     
+    // routes
+    const routes = [
+        {
+            id: 1,
+            title: "Shop",
+            href: "/shop"
+        },
+
+        {
+            id: 2,
+            title: "Blog",
+            href: "/shop"
+        },
+
+        {
+            id: 3,
+            title: "Parfume",
+            href: "/shop"
+        },
+
+        {
+            id: 4,
+            title: "Gallery",
+            href: "/shop"
+        },
+       
+
+
+    ];
+
+
+  
+
+
     return (
       <div className="w-full flex items-center justify-between px-12 py-4 mt-4 relative z-10">
     
@@ -19,14 +55,28 @@ import {
             alt="logo"
             width={120}
             height={120}
-            className="absolute -top-2"
+            className="absolute -top-6"
           />
         </div>
-  
+
+        <nav>
+        <div className='flex items-center ml-12 gap-12 text-md uppercase text-slate-800'>
+            {routes.map((route) => (
+                <div key={route.id}>
+                   <Link href = {route.href}>{route.title}</Link>
+                   
+                    </div>
+            ))}
+        </div>
+        </nav>
   
         <SignedIn>
-          <div className="flex items-center gap-6">
-            
+          <div className="flex items-center gap-6 ">
+            <div className='flex items-center gap-4 bg-black text-white w-[110px] h-[44px] rounded-full px-4 cursor-pointer transition hover:opacity-90 shadow-lg'>
+          <ShoppingCart size={20} />
+          <div className="w-px h-6 bg-white opacity-40" />
+          <Heart size={20} />
+          </div>
             <UserButton
               appearance={{
                 elements: {
