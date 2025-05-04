57bb2ad16d76be164c474eec52fe23e57e12f158
luka chikashvili <lukachikashvili2020@gmail.com>
2025-05-05 03:04:47 +0400

    finish hero section and framer motion animations

--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,22 +1,69 @@
 "use client"
+import { AnimatedText } from "@/components/AnimatedText";
 import Experience from "@/components/Experience";
 import Lights from "@/components/Lights";
 import { Canvas } from "@react-three/fiber";
-
+import { motion } from 'framer-motion'
 
 export default function Home() {
   return (
    <>
-    <section className="relative w-full h-screen  overflow-hidden ">
+    <section className="relative w-full h-screen flex items-center gap-12 justify-between px-12  overflow-hidden ">
      
-      l<Canvas className=" w-full -mt-12 ">
+    <div className="w-[65rem] -mt-36 flex flex-col gap-8">
+    <AnimatedText
+  text="Discover Your Perfect Fragrance, Fashion & More"
+  className="text-6xl font-semibold leading-[5rem]"
+  delay={0.6}
+/>
+
+  <motion.span initial = {{width: 0}} 
+  animate = {{width: "100%"}}
+  transition={{duration: 0.9, delay: 1.2, ease: "circIn"}}
+   className="w-full h-[0.2px] bg-black"></motion.span>
+
+  <AnimatedText
+  text="From Perfumes to Fashion, Find Everything You Love"
+  className="text-xl"
+  delay={1.2}
+/>
+
+
+<motion.button
+  initial={{ opacity: 0, y: 20 }}
+  animate={{ opacity: 1, y: 0 }}
+  transition={{
+    duration: 0.6,
+    ease: "easeOut",
+    delay: 1.6,
+  }}
+  className="learn-more"
+>
+  <span className="circle" aria-hidden="true">
+    <span className="icon arrow"></span>
+  </span>
+  <span className="button-text">Start shopping now</span>
+</motion.button>
+</div>
+     
+
+    
+      <Canvas className=" w-full  bg-transparent mt-6 rounded-lg  ">
          <Lights />
         <Experience />
       </Canvas>
-      
+    
       
      
     
+    </section>
+
+    <section className=" w-full h-screen flex items-center gap-12 justify-between px-12 ">
+    <AnimatedText
+  text="Discover Your Perfect Fragrance, Fashion & More"
+  className="text-6xl font-semibold leading-[5rem] w-1/2 m-auto"
+  delay={0.6}
+/>
     </section>
    </>
   );
--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,37 +1,55 @@
-import { Float,  useGLTF, useMatcapTexture } from '@react-three/drei'
+import { Float,  Stars,  useGLTF, useMatcapTexture } from '@react-three/drei'
 import { useLoader } from '@react-three/fiber';
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { MeshMatcapMaterial, TextureLoader } from 'three';
 
 const Experience = () => {
     const model = useGLTF('./model.glb');
     const matcap = useLoader(TextureLoader, './reflect.jpg');
+    const matcap2 = useLoader(TextureLoader, './gold.jpg');
 
+    const model1Ref = useRef();
+    const model2Ref = useRef();
 
     useEffect(() => {
-        if (!matcap) return;
-
-       model.scene.traverse((child) => {
-      if (child.isMesh) {
-        console.log(child.name)
-         if(child.name === "Object_6") {
-            child.material = new MeshMatcapMaterial({ matcap })
-            child.material.needsUpdate = true
-         }
-
-       
-        
-       
-      }
-    })
-    }, [model,matcap])
+        if (!matcap || !matcap2) return;
+    
+        const setupMaterial = (model, matcap) => {
+          model.traverse((child) => {
+            if (child.isMesh && child.name === 'Object_6') {
+              child.material = new MeshMatcapMaterial({ matcap });
+              child.material.needsUpdate = true;
+            }
+          });
+        };
+    
+        if (model1Ref.current) setupMaterial(model1Ref.current, matcap);
+        if (model2Ref.current) setupMaterial(model2Ref.current, matcap2);
+      }, [matcap, matcap2]);
 
   return (
    <>
 
-   <Float>
-   <primitive object={model.scene} scale = {1.4} position = {[5, 0, 0]} rotation = {[0, -1, 0]} />
-   </Float>
+<Float>
+        <primitive
+          object={model.scene.clone()}
+          scale={1.4}
+          position={[3.5, 0, 0]}
+          rotation={[0, -1, 0]}
+          ref={model1Ref}
+        />
+      </Float>
+
+      
+      <Float>
+        <primitive
+          object={model.scene.clone()}
+          scale={1.4}
+          position={[-1, 0, -1.3]}
+          rotation={[0, 0.7, 0]}
+          ref={model2Ref}
+        />
+      </Float>
    </>
   )
 }
