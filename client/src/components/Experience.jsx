9b519dc95652213889ed6cdb47f0c1d18689ee70
luka chikashvili <lukachikashvili2020@gmail.com>
2025-07-09 21:49:14 +0400

    change car model

--- client/src/components/Experience.jsx
+++ client/src/components/Experience.jsx
@@ -1,12 +1,25 @@
-import { useGLTF } from '@react-three/drei'
-import React from 'react'
+import { Environment, useGLTF } from '@react-three/drei'
+import React, { useEffect } from 'react'
 
 const Experience = () => {
     const model = useGLTF('./car.glb');
 
+    useEffect(() => {
+        model.scene.traverse((child) => {
+            if(child.isMesh) {
+                
+                console.log(child.name);
+
+               
+            }
+        })
+    }, [model])
+
   return (
     <>
+  
   <primitive object={model.scene} />
+
     </>
   )
 }
